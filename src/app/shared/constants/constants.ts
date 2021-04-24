1f0bdba55c27d19945a5c4ae9913b179253e4478
Sidheshwar Kacharde <kacharde_s@India.XoriantCorp.com>
2021-04-24 19:47:07 +0530

    comment change

--- src/app/shared/constants/constants.ts
+++ src/app/shared/constants/constants.ts
@@ -8,6 +8,6 @@ export  class Constants{
     public static UPDATE_TOAST_TITLE : string = 'Saved';
     public static UPDATE_TOAST_MESSAGE : string = 'Record Successfully updated';
 
-    //API URL
+    //We can add this url to enviornment.ts file as well 
     public static BASE_URL = 'http://localhost:3000/contacts';
 }
